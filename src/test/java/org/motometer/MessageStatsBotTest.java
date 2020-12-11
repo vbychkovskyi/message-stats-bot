ed56b083b9a39bb818813a1ad7b16b51e2be34aa
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2020-12-11 16:01:46 +0000

    Initial commit

---
+++ src/test/java/org/motometer/MessageStatsBotTest.java
@@ -0,0 +1,21 @@
+package org.motometer;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class MessageStatsBotTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
